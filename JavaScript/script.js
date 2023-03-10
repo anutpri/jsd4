92b0e6b21713943db50d1c5767031c671c5605ed
anutpri <anut.pri@gmail.com>
2023-03-10 15:02:26 +0700

    test js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,53 @@
-function hello(){
+// function hello(){
 
-    alert("Hello World!"); 
-    console.log(document.getElementById("demo").innerHTML = "The button has been clicked");
-}
+//     alert("Hello World!"); 
+//     console.log(document.getElementById("demo").innerHTML = "The button has been clicked");
+// }
 
+// let string1 = "Hello World!";
+// console.log(string1);
+
+// let fristName = "Anut";
+// let lastName = "pringkasamchai";
+// let fullName = fristName + ' ' + lastName;
+// console.log(fullName);
+
+// let info = fristName + ' ' + lastName;
+// console.log(info);
+
+// let email = 'anut.pri@gmail.com';
+// let check = email.includes('@');
+// console.log(check);
+
+// let radius = 10;
+// const pi = 3.14;
+
+
+// let un;
+// console.log(un);
+
+// let null1 = null;
+// console.log(null1);
+
+// let selectfruit = ['apple', 'banana', 'orange'];
+
+// selectfruit[3] = 'coconut';
+// selectfruit[1] = 'pineapple';
+// console.log(selectfruit);
+// console.log(selectfruit.length);
+
+// let user = {
+//     fristName: 'Anut', 
+//     lastName: 'Pringkasamchai',
+//     phoneNum: "088-888-8888",
+//     email: 'anut.pri@gmail.com',
+//     skill:['html', 'css', 'js'],
+
+// }
+
+// user.phoneNum = '088-888-9999';
+// user.skill.push('sql');
+// console.log(user);
+// console.log(user.fristName,user.lastName);
+// console.log(user.skill);
+// console.log(user.skill[3]);
