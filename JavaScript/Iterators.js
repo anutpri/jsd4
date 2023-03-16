e103eb74823add3c04ce7d96a92b612fdd856a88
anutpri <anut.pri@gmail.com>
2023-03-16 12:05:41 +0700

    inser comment to Iterators.js

--- JavaScript/Iterators.js
+++ JavaScript/Iterators.js
@@ -1,5 +1,7 @@
 
 // .forEach select all item in array start form index 0 by use callback function inside ()
+//is used to execute the same code on every element in an array 
+//but does not change the array and returns undefined.
 const artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
 
 artists.forEach(artist => {
@@ -14,6 +16,7 @@ fruits.forEach(fruits => console.log('I want to eat a '+fruits));
 
 
 //.map compute all item in array start form index 0 by use callback function inside () then show in new array
+//executes the same code on every element in an array and returns a new array with the updated elements.
 const numbers = [1, 2, 3, 4, 5];
 
 const squareNumbers = numbers.map(number => {
@@ -24,6 +27,8 @@ console.log(squareNumbers);
 
 
 //.filter check all item that match condition start form index 0 by use callback function inside ()
+//checks every element in an array to see if it meets certain criteria and returns a new array 
+//with the elements that return truthy for the criteria.
 const things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
 const onlyNumbers = things.filter(thing => {
     return typeof thing === 'number';
@@ -42,6 +47,8 @@ console.log(joinedStr); // Output: "The quick brown fox"
 
 
 //.findIndex find index that match condition then return index number, start from index 0(stop when found) by use callback function inside ()
+//returns the index of the first element of an array that satisfies a condition in the callback function. 
+//It returns -1 if none of the elements in the array satisfies the condition.
 const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
 
 const foundAnimal = animals.findIndex(animals => {
@@ -75,3 +82,13 @@ const allEven = arr1.every((currentValue) => {
 });
 
 console.log(allEven); // Output: true
+
+
+//.reduce iterates through an array and takes the values of the elements and returns a single value.
+const arr2 = [1, 2, 3, 4, 5];
+
+const sum = arr2.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+});
+
+console.log(sum); // Output: 15
