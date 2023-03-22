508b294bb843d5424a18317e655d0202f4380dab
anutpri <anut.pri@gmail.com>
2023-03-22 16:32:10 +0700

    test export

---
+++ JavaScript/exports.js
@@ -0,0 +1,26 @@
+// export class User {
+//     constructor(userID, password, fName, lName, age) {
+//       this.userID = userID;
+//       this.password = password;
+//       this.fName = fName;
+//       this.lName = lName;
+//       this.age = age;
+//     }
+  
+//     logon() {
+//       console.log('Welcome');
+//     }
+  
+//     logout() {
+//       console.log('Goodluck');
+//     }
+//   }
+  
+
+export function sum(a, b) {
+    return a + b;
+  }
+  
+  export function multiply(a, b) {
+    return a * b;
+  }
---
+++ JavaScript/toImport.js
@@ -0,0 +1,4 @@
+import { sum, multiply } from './exports.js';
+
+console.log(sum(1, 2)); 
+console.log(multiply(3, 4));
