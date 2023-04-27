da68aeedb7fe06ec32caa2c5c642c6ad204553c0
anutpri <anut.pri@gmail.com>
2023-04-27 11:55:16 +0700

    finish get post delete

--- Expressjs/index.js
+++ Expressjs/index.js
@@ -2,28 +2,49 @@
 const express = require('express');
 const app = express();
 
-// const userActivities = [];
+const userActivities = [];
 
 //Wlidcard stype
 app.get('/:para', (req, res) => {
     const {para} = req.params;
+    const activityData = userActivities.map(activity => `
+    ID: ${activity.id}
+    Activity Name: ${activity.activityName}
+    Description: ${activity.description}
+    Start Time: ${activity.startDateTime.toLocaleString()}
+    Finish Time: ${activity.finishDateTime.toLocaleString()}
+    Activity Type: ${activity.activityType}
+    Duration: ${activity.durationTime}
+    Distance: ${activity.distance}
+`);
     try {if (para === 'Main'){
-        res.send('This is Main Page');
+        
+        res.send(`All Activity : ${activityData}`);
 
-    } else if (para === 'Create'){
-    //     const newUserActivity = {
-    //         id: userActivities.length + 1,
-    //         activityName: 'Run',
-    //         description: 'Run with your Mom',
-    //         startDateTime: new Date(Date.now()),
-    //         finishDateTime: new Date(Date.now() + 3600000),
-    //         activityType: 'Run',
-    //         durationTime: '50 minutes',
-    //         distance: '5 KM'
-    // };
-    //     userActivities.push(newUserActivity);
-    //     res.send(newUserActivity);
-    res.send('This is Create Page');
+    } 
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/:para', (req, res) => {
+    const {para} = req.params;
+    try {if (para === 'Create'){
+        const newUserActivity = {
+            id: userActivities.length,
+            activityName: 'Run',
+            description: 'Run with your Mom',
+            startDateTime: new Date(Date.now()),
+            finishDateTime: new Date(Date.now() + 3600000),
+            activityType: 'Run',
+            durationTime: '50 minutes',
+            distance: '5 KM'
+    };
+        userActivities.push(newUserActivity);
+        res.send(`New Activity has been add ID:${newUserActivity.id}`);
+    
     }
 
     } catch (error) {
@@ -32,11 +53,29 @@ app.get('/:para', (req, res) => {
     }
 });
 
+app.delete('/:para/:activityID', (req, res) => {
+    const {para,activityID} = req.params;
+    const activityIndex = userActivities.findIndex((activity) => activity.id == activityID);
+    try {if (activityIndex){
+        userActivities[activityIndex] = null;
+        res.send(`Your Activity has been deleted`);
+    };
+       
+        
+        res.status(404),res.send('Activity not exist');
+    }
+
+    catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 //Normol style
-// app.get('/Main', (req, res) => {
+// app.get('/Delete', (req, res) => {
 //     try {
         
-//         res.send('This is Main Page');
+//         res.send('This is Delete Page');
                  
 //     } catch (error) {
 //         console.error(error);
@@ -44,26 +83,16 @@ app.get('/:para', (req, res) => {
 //     }
 // });
 
-// app.get('/Create', (req, res) => {
+// app.get('/Edit', (req, res) => {
 //     try {
-//         const newUserActivity = {
-//             id: userActivities.length + 1,
-//             activityName: 'Run',
-//             description: 'Run with your Mom',
-//             startDateTime: new Date(Date.now()),
-//             finishDateTime: new Date(Date.now() + 3600000),
-//             activityType: 'Run',
-//             durationTime: '50 minutes',
-//             distance: '5 KM'
-//         };
-//         userActivities.push(newUserActivity);
-//         res.send(newUserActivity);
+//         res.send('This is Edit Page');
+        
 //     } catch (error) {
 //         console.error(error);
 //         res.status(500).send('Internal Server Error');
 //     }
 // });
 
-app.listen(8082, () => {
-    console.log('Server listening on port 8082');
+app.listen(8084, () => {
+    console.log('Server listening on port 8084');
 });
